8f4e8c4ec82f4edd443bfbabbdf5438251bc13d3
jorge bernal <jorgebernal08041999@gmail.com>
2021-05-10 10:20:58 -0500

    Primera prueba

---
+++ PruebaGitV1/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PruebaGitV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+
+            Label l = new Label();
+            l.Text = "ESTO ES UNA PRUEBA";
+            l.Width = 150;
+            l.Height = 200;
+            l.ForeColor = Color.Red;
+            l.TextAlign = ContentAlignment.MiddleCenter;
+
+            this.Controls.Add(l);
+            
+        }
+
+        #endregion
+    }
+}
+
